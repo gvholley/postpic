546b19e369beb0670b5eac9cf88dd581bd848961
Grant Holley <gvholley@gmail.com>
2021-01-11 21:46:42 +0900

    Edited photo uploader

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user
+  #before_action :authenticate_user
   before_action :find_post, only: [:show]
 
   def index
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
     if @post.save
       if params[:images]
         params[:images].each do |img|
-          @post.photos.create(image: params[:images][img])
+          @post.photos.create(image: img)
         end
       end
 
@@ -22,6 +22,10 @@ class PostsController < ApplicationController
       flash[:alert] = "Something went wrong ..."
       redirect_to posts_path
     end
+  end
+
+  def show
+    @photos = @post.photos
   end
 
  private
